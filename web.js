725453316b08125127e05198a342a641cd340679
Cyprian Ziolkowski <ziouas@gmail.com>
2013-07-12 00:27:50 +0200

    Updated web.js with file read

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var buffer = new Buffer(100);
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  buffer.write(fs.readFileSync('./index.html'),"utf-8");
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
