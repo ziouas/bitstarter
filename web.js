7309d3baf71675402e66b017d4aae8aff19687a8
Cyprian Ziolkowski <ziouas@gmail.com>
2013-07-13 12:20:41 +0200

    Updated web.js with buffer

--- web.js
+++ web.js
@@ -3,14 +3,14 @@ var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
-var buffer = new Buffer(100);
+var buffer = new Buffer(100); 
 
 app.get('/', function(request, response) {
-  buffer.write(fs.readFileSync('./index.html'),"utf-8");
+  buffer.write(fs.readFileSync('./index.html','utf-8'));
   response.send(buffer.toString('utf-8'));
 });
 
-var port = process.env.PORT || 5000;
+Var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
