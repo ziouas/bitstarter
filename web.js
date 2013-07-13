42cdfea3b49c750fe9db4b592522d7359f888434
Cyprian Ziolkowski <ziouas@gmail.com>
2013-07-13 12:42:45 +0200

    Really final w02/t01 commit

--- web.js
+++ web.js
@@ -9,7 +9,7 @@ app.get('/', function(request, response) {
   var txt = fs.readFileSync('./index.html','utf-8');
   var len = txt.length;
   buffer.write(txt,0,len);
-  response.send(buffer.toString('utf-8'),0,len);
+  response.send(buffer.toString('utf-8',0,len));
 });
 
 var port = process.env.PORT || 5000;
