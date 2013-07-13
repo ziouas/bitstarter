b962d6e1382524a2a1a3b53666a31471127151e7
Cyprian Ziolkowski <ziouas@gmail.com>
2013-07-13 12:38:16 +0200

    Final w02/t01 commit

--- web.js
+++ web.js
@@ -6,7 +6,9 @@ var app = express.createServer(express.logger());
 var buffer = new Buffer(100); 
 
 app.get('/', function(request, response) {
-  var len = buffer.write(fs.readFileSync('./index.html','utf-8'));
+  var txt = fs.readFileSync('./index.html','utf-8');
+  var len = txt.length;
+  buffer.write(txt,0,len);
   response.send(buffer.toString('utf-8'),0,len);
 });
 
