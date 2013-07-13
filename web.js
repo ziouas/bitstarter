c9896ac8a5af5862b9733da891ad8efed5bbb4be
Cyprian Ziolkowski <ziouas@gmail.com>
2013-07-13 12:28:18 +0200

    Testing

--- web.js
+++ web.js
@@ -6,9 +6,8 @@ var app = express.createServer(express.logger());
 var buffer = new Buffer(100); 
 
 app.get('/', function(request, response) {
-  //buffer.write(fs.readFileSync('./index.html','utf-8'));
-  //response.send(buffer.toString('utf-8'));
-  response.send("Hello World2");
+  buffer.write(fs.readFileSync('./index.html','utf-8'));
+  response.send(buffer.toString('utf-8'));
 });
 
 var port = process.env.PORT || 5000;
